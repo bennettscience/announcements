cfc8bd10e2a0fb9a7309307cabf2377c13d3da61
bennettscience <brian.bennett2@gmail.com>
2016-07-29 06:46:30 -0400

    persistent background script, can edit source ssId

--- background.js
+++ background.js
@@ -1,41 +1,12 @@
+$(document).ready(function() {
+	chrome.runtime.onMessage.addListener(function(msg,sender,response) {
+		if((msg.from == "popup") && (msg.d == "update")) {
+			console.log("received");
+			response({do: "updateFeed"});
+		}
+	})
 // This runs all of the background work necessary to display information
 // in the popup.html file.
-$(document).ready(function() {
-  chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-    if((msg.from === "popup") && (msg.subject === "ssId")) {
-      checkJsonFeed();
-    }
-  });
-  // Grab the JSON data and print into the popup as HTML
-    function checkJsonFeed() {
-      var ssId = chrome.storage.sync.get("spreadsheetId", function() {
-        console.log("The key is: ");
-      })
-
-      // If the spreadsheetId is empty, run setup.
-        var url = "https://spreadsheets.google.com/feeds/list/" + ssId + "/od6/public/values?alt=json";
-
-        $.getJSON(url, function(data) {
-          var entry = data.feed.entry;
-
-          $(entry).each(function() {
-            $('#data').append('<h2>'+this.gsx$title.$t+'</h2><p>'+this.gsx$text.$t+'</p>');
-          });
-
-          // If there has been an update in the jsonTimestamp, add a badge
-          // to the browserAction icon
-          if(isUpdated(data.feed.updated.$t)) {
-            chrome.browserAction.setBadgeText({text: "NEW"});
-          }
-        });
-      }
-
-// Display the current date
-  var d = new Date();
-  var month = d.getMonth()+1;
-  var day = d.getDate();
-  var output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + d.getFullYear();
-  $('#date').append('<h1>'+output+'</h1>');
 
 // Check the JSON update key in localstorage
   function isUpdated(jsonTimestamp) {
@@ -50,18 +21,40 @@ $(document).ready(function() {
     // If there is a stamp and it equals the last JSON stamp, stop.
     // If it's not the same, update the stamp.
     if (storedTimestamp == jsonTimestamp) {
+		console.log("There's no new data, quitting.")
       return false;
     } else {
+		console.log("New data! Restting the timestamp.")
       localStorage.setItem("last-updated", jsonTimestamp);
-      console.log("The JSON time is: "+jsonTimestamp);
-      console.log("The localStorage is: "+storedTimestamp);
+      console.log("The new localStorage time is: "+jsonTimestamp);
       return true;
     }
   }
+	function checkJsonFeed() {
+		console.log("Checking the JSON timestamp data...")
+		chrome.storage.sync.get("d", function(items) {
+			var ssId = items.d;
+			var url = "https://spreadsheets.google.com/feeds/list/" + ssId + "/od6/public/values?alt=json";
 
-
-  // setup();
-  // checkJsonFeed();
+			$.getJSON(url, function(data) {
+				var entry = data.feed.entry;
+				
+				chrome.runtime.sendMessage({msg: "print"}, function() {
+					console.log("Message sent")
+				})
+				// $(entry).each(function() {
+					// $('#data').append('<h2>'+this.gsx$title.$t+'</h2><p>'+this.gsx$text.$t+'</p>');
+				// });
+				
+				// If there has been an update in the jsonTimestamp, add a badge
+				// to the browserAction icon
+				if(isUpdated(data.feed.updated.$t)) {
+					console.log("There's been an update. Setting the badge.");
+					chrome.browserAction.setBadgeText({text: "NEW"});
+				}
+			});
+		})        
+    }
 
 // Timer to check the JSON `updated` data.
   chrome.alarms.create("checkJsonFeed", {
@@ -71,9 +64,10 @@ $(document).ready(function() {
 
   // Check the spreadsheet feed every minute looking for a change
   // in the update data.
-  chrome.alarms.onAlarm.addListener(function(alarm) {
-    checkJsonFeed();
-  });
+    chrome.alarms.onAlarm.addListener(function(alarm) {
+		console.log("Checking feed...")
+		checkJsonFeed();
+	});
 
   // Check the badge on a click - if there is text, remove the badge.
   function updateBadge() {
@@ -88,4 +82,5 @@ $(document).ready(function() {
   // by callback functions for security purposes.
   chrome.browserAction.onClicked.addListener(updateBadge);
   updateBadge();
+
 });
--- popup.js
+++ popup.js
@@ -1,19 +1,74 @@
-$(document).ready(function() {
-  // Link the spreadsheet to the extension
-  function setup() {
-    var spreadsheetId = "";
-    $("#setup").append('<form><input type="text" id="ssId" value="Enter key here" /><br /><input type="submit" value="set" id="set" />');
-    $("#set").click(function() {
-      event.preventDefault();
-      var ssId = $("#ssId").val();
-      chrome.storage.sync.set({"spreadsheetId": ssId}, function() {
-        chrome.runtime.sendMessage({
-          from:     'popup',
-          subject:  'ssId'
-        });
-        console.log("Message posted with: "+ssId);
-      });
+// Send a message to update the feed
+chrome.runtime.sendMessage({from: "popup", d: "update"}, function(response) {
+	if(response.do == "updateFeed") {
+		checkJsonFeed();
+	}
+});
+	// Check for a document ID stored in the Chrome StorageArea
+	chrome.storage.sync.get("d", function(items) {
+		if(items.d === undefined) {
+			var error = chrome.runtime.lastError;
+			console.log(error)
+			setup();
+			$("#clear-ssId").hide();
+		} else {
+			console.log("The current key is: " + items.d);
+			$("#setup").hide();
+			$("#clear-ssId").show();
+		}
+	})
+	
+	// Clear the spreadsheet ID
+	$("#clear-ssId").click(function() {
+		// event.preventDefault();
+		chrome.storage.sync.clear( function() { console.log("Key cleared"); setup() });
+	})
+    function checkJsonFeed() {
+		chrome.storage.sync.get("d", function(items) {
+			var ssId = items.d;
+			var url = "https://spreadsheets.google.com/feeds/list/" + ssId + "/od6/public/values?alt=json";
+
+			$.getJSON(url, function(data) {
+				var entry = data.feed.entry;
+				
+				$(entry).each(function() {
+					$('#data').append('<h2>'+this.gsx$title.$t+'</h2><p>'+this.gsx$text.$t+'</p>');
+				});
+			});
+		})        
+    }
+	  
+	// Link the spreadsheet to the extension
+	function setup() {
+		// var ssId = "";
+		$("#set").click(function() {
+			event.preventDefault();
+			var ssId = $("#ssId").val();
+			chrome.storage.sync.set({"d": ssId}, function() {
+				console.log("Set in storage: "+ssId);
+			});
+		checkJsonFeed();
+		});
+	}
+
+// Display the current date
+  var d = new Date();
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+  var output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + d.getFullYear();
+  $('#date').append('<h1>'+output+'</h1>');
+
+  // Check the badge on a click - if there is text, remove the badge.
+  function updateBadge() {
+    chrome.browserAction.getBadgeText({}, function(result) {
+      if(result === "NEW") {
+        chrome.browserAction.setBadgeText({text: ""});
+      }
     });
-  }
-  setup();
-})
+  };
+
+  // Run the updateBadge script. Chrome forces events to be separated out
+  // by callback functions for security purposes.
+  chrome.browserAction.onClicked.addListener(updateBadge);
+  updateBadge();
+  
